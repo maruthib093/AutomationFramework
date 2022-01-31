d0ada05f870e4f7f64da9693db947854acbace22
arjun.mannambeth <arjun.mannambeth@nutanix.com>
2022-02-01 00:29:59 +0530

    framework update

--- src/main/java/com/extend/test/ArkTestListener.java
+++ src/main/java/com/extend/test/ArkTestListener.java
@@ -5,22 +5,25 @@ import org.testng.ITestListener;
 import org.testng.xml.XmlSuite;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ArkTestListener implements ITestListener, IAlterSuiteListener {
 
     @Override
     public void alter(List<XmlSuite> suiteList) {
-        List<String> groups=new ArrayList<>();
-
-        groups.add("login");
-        groups.add("signup");
-        for (XmlSuite suite : suiteList){
+        //List<String> groupMasterList = Arrays.asList("editQuoteLine", "addQuoteLine");
+        List<String> groupMasterList = Arrays.asList("login", "signup");
+        String commitMsg = System.getProperty("commitMsg", "edit login etc addQuoteLine");
+        String[] words = commitMsg.split(" ");
+        List<String> groups = new ArrayList<>();
+        for (String word : words) {
+            if (groupMasterList.contains(word))
+                groups.add(word);
+        }
+        for (XmlSuite suite : suiteList) {
             suite.setIncludedGroups(groups);
-            System.out.println("Setting groups ************");
-            System.out.println("Groups : "+suite.getGroups());
-            System.out.println("Groups included  : "+suite.getIncludedGroups());
-            System.out.println("Groups excluded : "+suite.getExcludedGroups());
+            System.out.println("Setting groups : " + groups);
         }
     }
 
